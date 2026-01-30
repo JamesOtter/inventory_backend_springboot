0ebb2a37e66c937d5c006d05329891c12a3d2e29
James <jamestan21403@gmail.com>
2026-01-30 18:20:27 +0800

    Added "cross-origin" - to accept api request from frontend http url

--- src/main/java/com/inventory/inventory_backend/controller/AuditLogController.java
+++ src/main/java/com/inventory/inventory_backend/controller/AuditLogController.java
@@ -4,6 +4,7 @@ import com.inventory.inventory_backend.model.AuditLog;
 import com.inventory.inventory_backend.service.AuditLogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 @RequestMapping("/api/admin/audit-logs")
 @RequiredArgsConstructor
 @PreAuthorize("hasRole('ADMIN')")       // only admin can access
+@CrossOrigin("http://localhost:5173")
 public class AuditLogController {
 
     private final AuditLogService auditLogService;
